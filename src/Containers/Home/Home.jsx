2d6fe17d7273e6a2b10c4c2c88ae0cb8220303a9
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-05-08 20:43:17 +0100

    Hero section completed

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Navbar } from "../../Components";
+import { Link } from "react-router-dom";
+
+import "./Home.scss";
 
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
+			<div className="app__home">
+				<div className="app__home_content">
+					<h2>Travel The World Of Tech With US</h2>
+					<p>Join the LearnDev Academy and explore the endless possibilities of the tech world. Our courses provide you with the skills you need to succeed in the ever-changing landscape of the industry</p>
+					<Link to="/academy">Get Started</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
