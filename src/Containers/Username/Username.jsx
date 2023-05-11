e527cee1eca5541d8781f4704d95c341925d5c5b
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-05-11 13:17:38 +0100

    [feat] Modified the username screen.
    Note: As at this commit The screen isn't responsive yet. Still to work
    on it before the next commit.
    
    Fonts added:
    - Poppins

--- src/Containers/Username/Username.jsx
+++ src/Containers/Username/Username.jsx
@@ -1,68 +1,66 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom'
-import { images } from '../../Constants';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Navbar, Footer } from "../../Components";
 import { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
-import { usernameValidate } from '../../helpers/validate';
-import { useAuthStore } from '../../store/store';
+import { usernameValidate } from "../../helpers/validate";
+import { useAuthStore } from "../../store/store";
 
 import "./Username.scss";
 
 const Username = () => {
 
-  const navigate = useNavigate();
-  const setUsername = useAuthStore(state => state.setUsername);
+	const navigate = useNavigate();
+	const setUsername = useAuthStore(state => state.setUsername);
 
-  const formik = useFormik({
-    initialValues: {
-      username : ''
-    },
-    validate: usernameValidate,
-    validateOnBlur: false,
-    validateOnChange: false,
-    onSubmit: async values => {
-      setUsername(values.username);
-      navigate('/password');
-    }
-  })
+	const formik = useFormik({
+		initialValues: {
+			username : ""
+		},
+		validate: usernameValidate,
+		validateOnBlur: false,
+		validateOnChange: false,
+		onSubmit: async values => {
+			setUsername(values.username);
+			navigate("/password");
+		}
+	});
 
-  return (
-    <div>
+	return (
+		<div>
+			<Navbar />
+			<Toaster position='bottom-center' reverseOrder={false}></Toaster>
+			<div className="app__username">
+				<div className="app__username_content">
+					<div className="app__username_content-title">
+						<h3>Login</h3>
+						<p>Don&apos;t have an account? <Link to="/register">Sign Up</Link></p>
+					</div>
+					<div className="app__username_content-stepper">
+						<div className="one">
+							<div className="number">
+								<p>1</p>
+							</div>
+							<p>Enter Your Username</p>
+						</div>
+						<div className="two">
+							<div className="number">
+								<p>2</p>
+							</div>
+							<p>Enter Your Password</p>
+						</div>
+					</div>
+					<form action="" onSubmit={formik.handleSubmit}>
+						<p>Enter Your Username:</p>
+						<input {...formik.getFieldProps("username")} type="text" placeholder='Username' />
+						<button type="submit">Proceed</button>
+					</form>
+				</div>
 
-      <div className='app__username container mx-auto'>
+			</div>
+			<Footer />
+		</div>
+	);
+};
 
-        <Toaster position='bottom-center' reverseOrder={false}></Toaster>
-
-        <div className="flex justify-center items-center h-screen">
-            
-            <div className='app__username-glass'>
-
-              <div className="title flex flex-col items-center">
-                <h4 className="text-5xl font-bold">Welcome Back</h4>
-                <span className="py-4 text-xl w-2/3 text-center text-gray-500">Type in your username to login</span>
-              </div>
-
-              <form action="" className="py-1" onSubmit={formik.handleSubmit}>
-                <div className="profile flex justify-center py-4">
-                  <img src={images.avatar} className='app__username-profileimg' alt="avatar" />
-                </div>
-
-                <div className="textbox flex flex-col items-center gap-6">
-                  <input {...formik.getFieldProps('username')} type="text" className="app__username-textbox" placeholder='Username' />
-                  <button className='app__username-btn' type="submit">Proceed</button>
-                </div>
-
-                <div className="text-center py-4">
-                  <span className='text-gray-500'>Don't have an account? <Link className='text-red-500' to="/register">Register</Link> </span>
-                </div>
-              </form>
-
-            </div>
-
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default Username
+export default Username;
